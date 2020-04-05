632e2b95431d1d0ebba223be50b55af009884ceb
arpanpaliwal <paliwal.arpan@gmail.com>
2020-04-05 17:00:35 +0530

    added sorting to datatable

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
 
@@ -8,13 +9,16 @@ import { TrainingService } from '../training.service';
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css']
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
+  @ViewChild(MatSort) sort: MatSort;
   constructor(private trainingService: TrainingService) { }
-
   ngOnInit(): void {
     this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
   }
 
 }
