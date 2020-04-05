326aa3479941a0233f8a1377aa67001ff92e7e22
arpanpaliwal <paliwal.arpan@gmail.com>
2020-04-05 17:16:19 +0530

    --start-- added paginator to datatable --end--

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-past-training',
@@ -13,12 +14,14 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(private trainingService: TrainingService) { }
   ngOnInit(): void {
     this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
   }
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   doFilter(val: string){
